42106081dde605f8d2df3ae06455b38bfbe03ab5
ZEN <zen@ctrlworks.com>
2019-12-10 20:46:06 +0800

    update log format

--- reload.py
+++ reload.py
@@ -1,8 +1,22 @@
+import sys
 import yaml
 import datetime
+import logging
 
 from pandas.io.json import json_normalize
 from yahoofinancials import YahooFinancials
+
+logformat = "%(asctime)s %(levelname)s %(module)s - %(funcName)s: %(message)s"
+datefmt = "%m-%d %H:%M"
+
+logging.basicConfig(filename="app.log", level=logging.INFO, filemode="w",
+                    format=logformat, datefmt=datefmt)
+
+stream_handler = logging.StreamHandler(sys.stderr)
+stream_handler.setFormatter(logging.Formatter(fmt=logformat, datefmt=datefmt))
+
+logger = logging.getLogger("app")
+logger.addHandler(stream_handler)
 
 def load_meta():
     with open('data/meta.yaml') as stream:
@@ -11,9 +25,6 @@ def load_meta():
 def main():
     metas = load_meta()
     for fname, meta in metas.iteritems():
-        print fname, meta
-        print '-' * 20
-
         code = meta['code']
         start = str(meta['start'])
         end = datetime.datetime.today().strftime('%Y-%m-%d')
@@ -37,6 +48,7 @@ def main():
                   index=False,
                   columns=['Date', 'Open', 'High', 'Low', 'Close',
                            'Adj Close', 'Volume'])
+        logger.info('Download {meta} - {fname}'.format(meta=meta, fname=fname))
 
 
 if __name__ == '__main__':
